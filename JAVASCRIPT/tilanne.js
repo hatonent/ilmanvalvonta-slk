13f24db2b86cc60fb73a65a406b8fcc59eaacd7d
Karjalainen Jetro <s1700583@student.businesscollege.fi>
2018-03-09 09:32:55 +0200

    uusin

--- JAVASCRIPT/tilanne.js
+++ JAVASCRIPT/tilanne.js
@@ -1,48 +1,4 @@
 
-/*
-var xhttp = new XMLHttpRequest();
-xhttp.onreadystatechange = function() {
-if (this.readyState == 4 && this.status == 200) {
-// Typical action to be performed when the document is ready:
-console.log(xhttp.responseText);
-var tiedot = JSON.parse(xhttp.responseText);
-/*	document.getElementById("demo").innerHTML = "Lämpötila: " + tiedot.temperature + " &deg;C" + "<br />" + "Ilmankosteus: " + tiedot.humidity + " &#37;" + "<br />" + "Hiilidioksidi: " + tiedot.CO2 + "<br />" + "Luokkatila: " + tiedot.areaId + "<br />" + "Sensori-ID: " + tiedot.sensorId + "<br />" + "Mittausaika: " + tiedot.timestamp;
-
-google.charts.load('current', {'packages':['gauge']});
-google.charts.setOnLoadCallback(drawChart);
-
-function drawChart() {
-
-var data = google.visualization.arrayToDataTable([
-['Label', 'Value'],
-['Ilmankosteus', tiedot.humidity],
-['Lämpötila', tiedot.temperature],
-['CO2', tiedot.CO2]
-]);
-
-var options = {
-
-min:0, max: 70,
-redFrom: 60, redTo: 70,
-yellowFrom:50, yellowTo: 60,
-minorTicks: 5
-};
-
-var chart = new google.visualization.Gauge(document.getElementById('chart_div'));
-
-chart.draw(data, options);
-
-}
-}
-};
-function getData(url) {
-xhttp.open("GET", url, true);
-xhttp.send();
-}
-
-getData("http://api.ruonavaara.fi/iot/area/5/latest");
-setInterval(getData,60000);
-*/
 var xhttp = new XMLHttpRequest();
 xhttp.onreadystatechange = function() {
 	if (this.readyState == 4 && this.status == 200) {
